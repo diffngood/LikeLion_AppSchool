5d720aeb0e0cd5e0cfba5496333b0b2538a2a42c
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-02-28 14:22:14 +0900

    24-02-28_commit

--- 02_Android/Android_memo/app/src/main/java/kr/co/lion/android_memo/MemoModel.kt
+++
@@ -1,3 +0,0 @@
-package kr.co.lion.android_memo
-
-data class MemoModel(var memoIdx: Int, var memoSubject: String, var memoText: String)
--- 02_Android/EX14_Memo/app/src/main/java/kr/co/lion/ex14_memo/MemoModel.kt
+++
@@ -1,3 +0,0 @@
-package kr.co.lion.ex14_memo
-
-data class MemoModel(var idx:Int, var title:String, var content:String)
